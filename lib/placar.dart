42f47eecd295da8e8b4bea60476ba4defadc39c1
Carlos Daniel <ms_danniel@outlook.com>
2023-10-18 01:11:22 -0300

    tentativa de responsividade

--- lib/placar.dart
+++ lib/placar.dart
@@ -1,4 +1,4 @@
-import 'package:fluter_proj1/main.dart';
+import 'package:YonScoreBoard/main.dart';
 import 'package:flutter/material.dart';
 
 
@@ -16,6 +16,8 @@ class Placar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
     //expanded para ocupar todo o restante da tela
     return Expanded(
       child: Row(
@@ -23,117 +25,133 @@ class Placar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center, 
 
         children: <Widget>[
-          SizedBox(
-            width: 60,
-            //botao de menos do time 1
-            child: IconButton(
-              onPressed: () { 
-                alterPoints(Teams.team1,-1);
-              },
-              iconSize: 50,
-              color:  Colors.red[900],
-              icon: const Icon(Icons.remove_circle_outline), 
-            ),
-          ),
-
-          SizedBox(
-            width: 60,
-            //botao de mais do time 1
-            child: IconButton(
-              onPressed: () { 
-                alterPoints(Teams.team1,1);
-              },
-              iconSize: 50,
-              color: Colors.green[900],
-              icon: const Icon(Icons.add_circle_outline), 
-            ),
-          ),
-
-          SizedBox(
-            width: 200,
-            //pontos do time 1
-            child: Text(
-                _teamOnePoints < 10 ? '0' + _teamOnePoints.toString() : _teamOnePoints.toString(),
-                style: const TextStyle(
-                  fontSize: 174,
-                  color:  Colors.white,
-                  decoration: TextDecoration.underline,
-                  //backgroundColor: Colors.amber,
-                ),
+          //botao de menos do time 1
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              child: IconButton(
+                onPressed: () { 
+                  alterPoints(Teams.team1,-1);
+                },
+                iconSize: 50,
+                color:  Colors.red[900],
+                icon: const Icon(Icons.remove_circle_outline), 
               ),
+            ),
           ),
+
+          //botao de mais do time 1
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              child: IconButton(
+                onPressed: () { 
+                  alterPoints(Teams.team1,1);
+                },
+                iconSize: 50,
+                color: Colors.green[900],
+                icon: const Icon(Icons.add_circle_outline), 
+              ),
+            ),
+          ),
+          
+          //pontos do time 1
+          Expanded(
+            flex: 4,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                  _teamOnePoints < 10 ? '0' + _teamOnePoints.toString() : _teamOnePoints.toString(),
+                  style: const TextStyle(
+                    fontSize: 210,
+                    color:  Colors.white,
+                    decoration: TextDecoration.underline,
+                    //backgroundColor: Colors.amber,
+                  ),
+                ),
+            ),
+          ),
+          
         
-          //digito de set do time 1
-          Container(
-            width: 50,
-            margin: EdgeInsets.fromLTRB(5, 20, 10, 0),
-            alignment: Alignment.topLeft,
-            //pontos do time 1
-            child: Text(
-                _teamOneSets.toString(),
-                style: const TextStyle(
-                  fontSize: 74,
-                  color:  Colors.white,
-                  //backgroundColor: Colors.amber,
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 10, 0),
+              alignment: Alignment.topLeft,
+              child: Text(
+                  _teamOneSets.toString(),
+                  style: const TextStyle(
+                    fontSize: 74,
+                    color:  Colors.white,
+                  ),
                 ),
-              ),
+            ),
           ),
 
           //digito de set do time 2
-          Container(
-            width: 50,
-            margin: EdgeInsets.fromLTRB(10, 20, 5, 0),
-            alignment: Alignment.topRight,
-            //pontos do time 1
-            child: Text(
-                _teamTwoSets.toString(),
-                style: const TextStyle(
-                  fontSize: 74,
-                  color:  Colors.white,
-                  //backgroundColor: Colors.amber,
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              alignment: Alignment.topRight,
+              child: Text(
+                  _teamTwoSets.toString(),
+                  style: const TextStyle(
+                    fontSize: 74,
+                    color:  Colors.white,
+                  ),
                 ),
-              ),
-          ),
-
-          SizedBox(
-            width: 200,
-            //pontos do time 1
-            child: Text(
-                _teamTwoPoints < 10 ? '0' + _teamTwoPoints.toString() : _teamTwoPoints.toString(),
-                style: const TextStyle(
-                  fontSize: 174,
-                  color: Colors.white,
-                  decoration: TextDecoration.underline,
-                  //backgroundColor: Colors.amber,
-                ),
-              ),
+            ),
           ),
           
-          SizedBox(
-            width: 60,
-            //botao de mais do time 2
-            child: IconButton(
-              onPressed: () { 
-                alterPoints(Teams.team2,1);
-              },
-              iconSize: 50,
-              color: Colors.green[900],
-              icon: const Icon(Icons.add_circle_outline),
-              
+          //pontos do time 2
+          Expanded(
+            flex: 4,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                  _teamTwoPoints < 10 ? '0' + _teamTwoPoints.toString() : _teamTwoPoints.toString(),
+                  style: const TextStyle(
+                    fontSize: 210,
+                    color: Colors.white,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
             ),
           ),
-          SizedBox(
-            width: 60,
-            //botao de menos do time 2
-            child: IconButton(
-              onPressed: (){
-                alterPoints(Teams.team2,-1);
-              },
-              iconSize: 50,
-              color:  Colors.red[900],
-              icon: const Icon(Icons.remove_circle_outline), 
+          
+          Expanded(
+            flex: 1,
+            child: Container(
+              //botao de mais do time 2
+              child: IconButton(
+                onPressed: () { 
+                  alterPoints(Teams.team2,1);
+                },
+                iconSize: 50,
+                color: Colors.green[900],
+                icon: const Icon(Icons.add_circle_outline),
+                
+              ),
             ),
           ),
+          
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              //width: 80,
+              //botao de menos do time 2
+              child: IconButton(
+                onPressed: (){
+                  alterPoints(Teams.team2,-1);
+                },
+                iconSize: 50,
+                color:  Colors.red[900],
+                icon: const Icon(Icons.remove_circle_outline), 
+              ),
+            ),
+          ),
+          
         ],
       )
     );
