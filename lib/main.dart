2df469589ab94ed76c31e17c8a2ba8eec3d32210
Carlos Daniel <ms_danniel@outlook.com>
2023-10-19 22:31:48 -0300

    remoção de metodos e variaveis. #1

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import './Placar.dart';
 
 main() {
   runApp(PlacarApp());
-} 
+}
 
-class _PlacarState extends State<PlacarApp>{
-
+class _PlacarState extends State<PlacarApp> {
   // ignore: unused_field
-  var _teamOnePoints = 0, _teamTwoPoints = 0, _teamOneSets = 0, _teamTwoSets = 0;
+  var _teamOnePoints = 0,
+      _teamTwoPoints = 0,
+      _teamOneSets = 0,
+      _teamTwoSets = 0;
   var _maxPoints = 15;
 
   @override
@@ -18,7 +20,7 @@ class _PlacarState extends State<PlacarApp>{
     super.initState();
     //define a orientação do app
     SystemChrome.setPreferredOrientations([
-      //DeviceOrientation.landscapeRight, 
+      //DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
       DeviceOrientation.landscapeRight
     ]);
@@ -26,51 +28,8 @@ class _PlacarState extends State<PlacarApp>{
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   }
 
-  void _incrementPoints(Teams team, int point){
-      //incrementa/decrementa a pontuação de algum dos times
-      switch(team){
-        case Teams.team1:
-          if((_teamOnePoints >= 0 && point > 0) || (point < 0 && _teamOnePoints > 0)){
-            setState(() {
-               _teamOnePoints += point;
-            });
-          }
-           
-          break;
-
-        case Teams.team2:
-          if((_teamTwoPoints >= 0 && point > 0) || (point < 0 && _teamTwoPoints > 0)){
-            setState(() {
-              _teamTwoPoints += point;
-            });
-          }  
-          break;
-
-        default:
-          break;
-      }
-
-      //quando atingir o maximo de pontos definido
-      //incremnta os sets e reseta a pontuação
-      if(_teamOnePoints >= _maxPoints){
-        setState(() {
-          _teamOneSets += 1;
-          _teamOnePoints = 0;
-          _teamTwoPoints = 0;
-        });
-      }
-      else if(_teamTwoPoints >= _maxPoints){
-        setState(() {
-          _teamTwoSets += 1;
-          _teamOnePoints = 0;
-          _teamTwoPoints = 0;
-        });
-      }
-  }
-
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         /*appBar: AppBar(
@@ -82,21 +41,21 @@ class _PlacarState extends State<PlacarApp>{
         body: Column(
           children: <Widget>[
             BarraSuperiorPlacar(),
-            Placar(_teamOnePoints, _teamOneSets, _teamTwoPoints, _teamTwoSets, _maxPoints, _incrementPoints),
+            Placar(_teamOnePoints, _teamOneSets, _teamTwoPoints, _teamTwoSets,
+                _maxPoints),
           ],
         ),
-        backgroundColor: Colors.black, 
+        backgroundColor: Colors.black,
       ),
     );
   }
 }
 
-class PlacarApp extends StatefulWidget{
+class PlacarApp extends StatefulWidget {
   const PlacarApp({super.key});
 
-
   @override
-  _PlacarState createState(){
+  _PlacarState createState() {
     return _PlacarState();
   }
 }
