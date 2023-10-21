6e7693a1f495b71080dab41fc1aa216ae49da851
Carlos Daniel <ms_danniel@outlook.com>
2023-10-20 23:28:24 -0300

    opção e dropdownbutton #3

--- lib/pages/configurations.dart
+++ lib/pages/configurations.dart
@@ -1,21 +1,71 @@
 import 'package:flutter/material.dart';
 
 class Configurations extends StatelessWidget {
-  const Configurations({super.key});
+  var dropdownValue = 15;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Configurações'),
-        ),
-        body: ListView(
-          children: const <Widget>[
-            ListTile(
-              title: Text('Pontuação máxima'),
-              subtitle: Text('Definir pontuação máxima para finalizar o set'),
+      appBar: AppBar(
+        title: const Center(child: Text('Configurações')),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(4.0), // Altura da borda
+          child: Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                    width: 1, color: Colors.white), // Estilo da borda
+              ),
             ),
-          ],
-        ));
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text(
+              'Pontuação máxima',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            subtitle:
+                const Text('Definir pontuação máxima para finalizar o set'),
+            textColor: Colors.white,
+            trailing: Container(
+              alignment: Alignment.center,
+              width: 60,
+              height: double.infinity,
+              child: DropdownButton(
+                dropdownColor: const Color.fromARGB(255, 60, 60, 60),
+                isExpanded: true,
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_drop_down_outlined),
+                elevation: 16,
+                onChanged: (int? value) {
+                  if (value != null) {
+                    dropdownValue =
+                        value; // criar função para renderizar a view
+                  }
+                },
+                items: List<DropdownMenuItem<int>>.generate(14, (index) {
+                  return DropdownMenuItem<int>(
+                    value: 12 + index,
+                    child: Text(
+                      '${12 + index}',
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Color.fromARGB(255, 11, 11, 11),
+    );
   }
 }
