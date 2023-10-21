e8350fad0af59d2a29d7682777a9e05080c80a90
Carlos Daniel <ms_danniel@outlook.com>
2023-10-20 21:45:48 -0300

    commented #3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:YonScoreBoard/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(PlacarApp());
+    await tester.pumpWidget(const PlacarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -26,5 +26,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
+  });*/
 }
