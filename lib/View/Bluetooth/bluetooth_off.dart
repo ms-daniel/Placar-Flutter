7e52deb87687c15f88cb0f8994a539e8df833e92
Carlos Daniel <ms_danniel@outlook.com>
2023-10-28 15:23:51 -0300

    tela bluetooth off. #223

---
+++ lib/View/Bluetooth/bluetooth_off.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import '../../Utils/snackbar.dart';
+
+class BluetoothOff extends StatelessWidget {
+  const BluetoothOff({Key? key, this.adapterState}) : super(key: key);
+
+  final BluetoothAdapterState? adapterState;
+
+  Widget buildBluetoothOffIcon(BuildContext context) {
+    return const Icon(
+      Icons.bluetooth_disabled,
+      size: 200.0,
+      color: Colors.white54,
+    );
+  }
+
+  Widget buildTitle(BuildContext context) {
+    String? state = adapterState?.toString().split(".").last;
+    return Text(
+      'Função Bluetooth ${state != null ? state : 'não disponível'}',
+      style: Theme.of(context).primaryTextTheme.titleSmall?.copyWith(color: Colors.white),
+    );
+  }
+
+  Widget buildTurnOnButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+        child: const Text('Ligar'),
+        onPressed: () async {
+          try {
+            if (Platform.isAndroid) {
+              await FlutterBluePlus.turnOn();
+            }
+          } catch (e) {
+            Snackbar.show(ABC.a, prettyException("Erro ao tentar ligar: ", e), success: false);
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      key: Snackbar.snackBarKeyA,
+      child: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildBluetoothOffIcon(context),
+              buildTitle(context),
+              if (Platform.isAndroid) buildTurnOnButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
